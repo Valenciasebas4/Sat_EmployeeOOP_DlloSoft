e1b635c2804b6d2d0a2b61da2ecaa87e8cea3776
SEBASTIAN LONDO�O VALENCIA <sebastianlondono264947@correo.itm.edu.co>
2023-09-16 11:27:49 -0500

    Create Class SalaryEmployee

---
+++ Sat_EmployeeOOP_DlloSoft/Sat_EmployeeOOP_DlloSoft/Classes/SalaryEmployee.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sat_EmployeeOOP_DlloSoft
+{
+    public class SalaryEmployee : Employee
+    {
+        #region Properties
+
+        public decimal Salary { get; set; }
+
+        #endregion
+
+        #region Methods
+        public override decimal GetValueToPay()
+        {
+            return Salary;            
+        }
+
+        public override string ToString()
+        {
+            return $"{base.ToString()}\n\t" +
+                $"Salario........{GetValueToPay():C2}";
+        }
+
+        #endregion
+    }
+}
